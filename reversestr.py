2812eebd02a07a4e8e875fe5980064fd5f8983b6
swolfson <swolfson2003@gmail.com>
2014-02-25 14:38:44 -0500

    reversestr function works

--- reversestr.py
+++ reversestr.py
@@ -3,6 +3,12 @@ from sys import argv
 import string
 #script, word, extra = argv
 
-def reverse(targetword):
+def reverse(targetstring):
+	return targetstring[::-1]
+
+if __name__=="__main__":
+	script, target = argv
+	print reverse(target)
+
 
 
